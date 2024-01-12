668af7924a0750da2f05963ba1a29a350912ff08
Francisco Fuentes <150530152+jav9sp@users.noreply.github.com>
2024-01-12 18:48:29 -0300

    cambios menores

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,16 @@
 import HeroPrincipal from '../components/HeroPrincipal'
 import Areas from '../components/Areas'
+import './Index.css'
 
 function Index() {
   return (
     <>
       <HeroPrincipal />
+
+      <div className='contenedor'>
+        <p>Ser adolescente y estar a puertas de salir del colegio puede ser todo un desafío.</p>
+      </div>
+
       <Areas />
 
     </>
